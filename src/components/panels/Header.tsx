2b75447d99b59300710b6e76df50b0be1a4d3191
kkukileon305 <kkukileon305@gmail.com>
2023-01-26 17:55:22 +0900

    아코디언 카테고리 메뉴 추가

--- src/components/panels/Header.tsx
+++ src/components/panels/Header.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { BsSearch, BsCart } from 'react-icons/bs';
+import { BsSearch, BsCart, BsArrowUp } from 'react-icons/bs';
 import ThemeSwitcher from '../ThemeSwitcher';
 import categories from '../../lib/categories';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isHover, setIsHover] = useState(false);
+  const categoryBtn = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const scrollHandler = () => {
@@ -33,18 +35,25 @@ const Header = () => {
       </header>
 
       <div className='flex items-center sticky top-0 bg-white dark:bg-slate-800 shadow-md'>
-        <div className='max-w-[1260px] h-[50px] mx-auto w-full px-4 py-2 flex justify-between items-center relative'>
-          <ul className='flex gap-4 text-red-500'>
-            {categories.map(category => (
-              <li key={category}>
-                <Link to={`/category/${encodeURIComponent(category)}`}>{category}</Link>
-              </li>
-            ))}
-          </ul>
+        <div className='max-w-[1260px] h-[50px] mx-auto w-full px-4 flex justify-between items-center relative'>
+          <button //
+            ref={categoryBtn}
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+            className='p-2 w-[160px] text-left h-[50px]'
+          >
+            <p className='font-bold text-red-500'>Category</p>
 
-          <Link to={'/random'} className='text-red-500'>
-            Get Random
-          </Link>
+            <ul className='absolute left-4 top-full text-red-500 bg-slate-100 dark:bg-slate-600 border-t border-slate-200 dark:border-slate-900 shadow-md' style={{ visibility: isHover ? 'visible' : 'hidden' }}>
+              {categories.map(category => (
+                <li key={category} className='text-left w-[160px] hover:bg-slate-400'>
+                  <Link className='w-full p-2 block' to={`/category/${encodeURIComponent(category)}`}>
+                    {category}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </button>
 
           {isScrolled && (
             <div className='flex items-center gap-8'>
@@ -56,6 +65,9 @@ const Header = () => {
           )}
 
           <div className='flex items-center gap-4'>
+            <Link to={'/random'} className='text-red-500 font-bold'>
+              Random
+            </Link>
             <ThemeSwitcher />
             <button>
               <BsCart size={24} color='red' />
@@ -63,6 +75,19 @@ const Header = () => {
           </div>
         </div>
       </div>
+      {isScrolled && (
+        <button //
+          className='fixed right-4 bottom-4 bg-red-400 rounded-full p-2 text-white'
+          onClick={() =>
+            window.scrollTo({
+              top: 0,
+              behavior: 'smooth',
+            })
+          }
+        >
+          <BsArrowUp size={30} />
+        </button>
+      )}
     </>
   );
 };
