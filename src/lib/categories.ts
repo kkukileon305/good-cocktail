ab21e7a6031b664cca02359407a9d46fa12ebea6
kkukileon305 <kkukileon305@gmail.com>
2023-01-26 20:17:49 +0900

    home 페이지에서 first letter 상태를 useState 에서 query string을 활용하도록 바꾸어 브라우저 히스토리에 남도록 변경

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -1,3 +1,3 @@
-const categories = ['Cocktail', 'Shot', 'Ordinary Drink', 'Shake', 'Other / Unknown', 'Punch / Party Drink', 'Soft Drink'];
+const categories = ['Cocktail', 'Shot', 'Ordinary Drink', 'Shake', 'Other / Unknown', 'Punch / Party Drink', 'Soft Drink', 'Coffee / Tea'];
 
 export default categories;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,20 +1,26 @@
 import { useState } from 'react';
+import { Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import DrinkItem from '../components/items/DrinkItem';
 import DrinkItemSkeleton from '../components/skeletons/DrinkItem.skeleton';
 import useFirstLetter from '../hooks/queries/useFirstLetter';
 import alphabets from '../lib/alphabets';
 
 const Home = () => {
-  const [firstLetter, setFirstLetter] = useState(alphabets[0]);
-  const { data: response, isLoading } = useFirstLetter(firstLetter);
+  const [searchParams] = useSearchParams();
+  const { data: response, isLoading } = useFirstLetter(searchParams.get('f') || 'A');
 
   return (
     <div>
-      <h2 className='font-bold text-xl text-center'>Search By First Letter, {firstLetter}</h2>
+      <h2 className='font-bold text-xl text-center'>Search By First Letter, {searchParams.get('f') || 'A'}</h2>
       <ul className='my-8 flex justify-center gap-4 flex-wrap'>
         {alphabets.map(alphabet => (
-          <li key={alphabet} onClick={() => setFirstLetter(alphabet)}>
-            <button className={`w-[24px] h-[24px] font-bold border border-slate-400 flex justify-center items-center rounded-full ${firstLetter === alphabet ? 'bg-transparent text-slate-400' : 'bg-slate-400 text-white'}`}>{alphabet}</button>
+          <li key={alphabet}>
+            <Link
+              to={`/?f=${alphabet}`}
+              className={`w-[24px] h-[24px] font-bold border border-slate-400 flex justify-center items-center rounded-full ${(searchParams.get('f') || 'A') === alphabet ? 'bg-transparent text-slate-400' : 'bg-slate-400 text-white'}`}
+            >
+              {alphabet}
+            </Link>
           </li>
         ))}
       </ul>
