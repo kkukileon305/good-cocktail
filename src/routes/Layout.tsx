fd19679c539541af153509c4159af64abb0c168a
kkukileon305 <kkukileon305@gmail.com>
2023-01-26 13:28:27 +0900

    search page 생성

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,19 +1,28 @@
-import { BsCart } from 'react-icons/bs';
-import SearchBox from '../components/SearchBox';
+import { BsCart, BsSearch } from 'react-icons/bs';
+import { Link, Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <>
-      <header>
+      <header className='border-b border-red-400'>
         <div className='max-w-[1260px] mx-auto p-4 flex justify-between items-center relative'>
-          <h1 className='font-bold text-3xl py-1'>Good App</h1>
-          <SearchBox />
+          <div className='flex items-center gap-8'>
+            <h1 className='font-bold text-3xl py-1'>
+              <Link to={'/'}>Good App</Link>
+            </h1>
+            <Link to={'/search'} className='flex gap-2 justify-start items-center border border-red-400 px-2 py-1 rounded '>
+              <p className='w-[100px] text-left'>검색...</p>
+              <BsSearch />
+            </Link>
+          </div>
+
           <button>
             <BsCart size={24} color='red' />
           </button>
         </div>
       </header>
-      <main>main</main>
+
+      <Outlet />
     </>
   );
 };
--- src/settings/router.tsx
+++ src/settings/router.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../routes/Layout';
+import Search from '../routes/Search';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    children: [
+      {
+        path: '/search',
+        element: <Search />,
+      },
+    ],
   },
 ]);
 
