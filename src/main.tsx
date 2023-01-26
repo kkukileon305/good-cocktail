60341ed9fbbe1cb65324dfe0c89316e5a508195b
kkukileon305 <kkukileon305@gmail.com>
2023-01-26 11:33:56 +0900

    init

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import queryClient from './settings/queryClient';
+import router from './settings/router';
+import './index.css';
+import { store } from './store/store';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
+);
---
+++ src/store/slices/cartSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  initialState: [],
+  name: 'cart',
+  reducers: {},
+});
+
+export const {} = cartSlice.actions;
+export default cartSlice.reducer;
