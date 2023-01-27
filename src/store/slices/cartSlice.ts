2ac3ed09309b5702410dc8abba489ec3f6feb24a
kkukileon305 <kkukileon305@gmail.com>
2023-01-28 02:35:32 +0900

    cart reducers 생성

--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -1,10 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type CartItem = {
+  idDrink: string;
+  strDrinkThumb: string;
+  strDrink: string;
+};
+
+const initialState: CartItem[] = [];
 
 const cartSlice = createSlice({
-  initialState: [],
+  initialState,
   name: 'cart',
-  reducers: {},
+  reducers: {
+    addCart: (state, { payload }: PayloadAction<CartItem>) => [...state, payload],
+    removeCart: (state, { payload }: PayloadAction<string>) => state.filter(item => item.idDrink !== payload),
+    removeAll: () => [],
+  },
 });
 
-export const {} = cartSlice.actions;
+export const { addCart, removeAll, removeCart } = cartSlice.actions;
 export default cartSlice.reducer;
