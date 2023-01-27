031962af05b37afa1e4de00b299a027ec965c921
kkukileon305 <kkukileon305@gmail.com>
2023-01-28 02:52:51 +0900

    drink item 카트 표시 구현

--- src/components/items/DrinkItem.tsx
+++ src/components/items/DrinkItem.tsx
@@ -1,12 +1,19 @@
+import { BsHeartFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../store/hooks';
 import { Drink, ShortDrink } from '../../types/response';
 
 const DrinkItem = ({ drink }: { drink: Drink | ShortDrink }) => {
+  const cart = useAppSelector(store => store.cart);
+
   return (
     <li className='border rounded w-1/5 min-w-[200px]'>
       <Link to={`/detail/${drink.idDrink}`} className='p-2 block'>
         <img src={drink.strDrinkThumb} alt={drink.strDrink} />
-        <p className='text-center my-2 font-bold'>{drink.strDrink}</p>
+        <div className='flex justify-center items-center gap-2'>
+          <p className='text-center my-2 font-bold'>{drink.strDrink}</p>
+          {cart.find(item => item.idDrink === drink.idDrink) && <BsHeartFill color='red' />}
+        </div>
       </Link>
     </li>
   );
