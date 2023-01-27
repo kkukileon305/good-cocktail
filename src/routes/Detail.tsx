e7ac652e6bd9174c28eeea6495022611a70dc5e4
kkukileon305 <kkukileon305@gmail.com>
2023-01-28 02:47:16 +0900

    cart에 추가 제거 toggle 기능 구현

--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,17 +1,38 @@
-import { BsArrowLeftShort } from 'react-icons/bs';
+import { BsArrowLeftShort, BsHeart, BsHeartFill } from 'react-icons/bs';
 import { useNavigate, useParams } from 'react-router-dom';
 import CategoryBtn from '../components/buttons/CategoryBtn';
 import DetailSkeleton from '../components/skeletons/Detail.skeleton';
 import useDetail from '../hooks/queries/useDetail';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { addCart, removeCart } from '../store/slices/cartSlice';
 
 const Detail = () => {
   const navigate = useNavigate();
   const { idDrink } = useParams();
   const { data: response, isLoading } = useDetail(idDrink || '12345');
 
+  const cart = useAppSelector(store => store.cart);
+  const dispatch = useAppDispatch();
+
   if (!response || isLoading) return <DetailSkeleton />;
 
   if (!response.drinks) return <div>No</div>;
+
+  const onClick = () => {
+    if (!idDrink || !response.drinks) return;
+
+    if (cart.find(item => item.idDrink === idDrink)) {
+      dispatch(removeCart(idDrink));
+    } else {
+      dispatch(
+        addCart({
+          idDrink,
+          strDrink: response.drinks[0].strDrink,
+          strDrinkThumb: response.drinks[0].strDrinkThumb,
+        })
+      );
+    }
+  };
 
   return (
     <div className='flex flex-col sm:flex-row gap-4 justify-between'>
@@ -22,10 +43,17 @@ const Detail = () => {
         <div className='flex flex-wrap gap-4 items-end'>
           <h2 className='font-bold text-3xl'>{response.drinks[0].strDrink}</h2>
           <CategoryBtn category={response.drinks[0].strCategory} />
+          <button onClick={onClick}>
+            {cart.find(item => item.idDrink === idDrink) ? ( //
+              <BsHeartFill color='red' size={32} />
+            ) : (
+              <BsHeart color='red' size={32} />
+            )}
+          </button>
         </div>
         <p className='mt-4'>{response.drinks[0].strInstructions}</p>
       </div>
-      <img src={response?.drinks[0].strDrinkThumb} alt='' className='w-[200px] mx-auto' />
+      <img src={response?.drinks[0].strDrinkThumb} alt={response?.drinks[0].strInstructions} className='w-[200px] mx-auto' />
     </div>
   );
 };
